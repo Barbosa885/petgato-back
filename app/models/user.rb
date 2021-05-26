f750161aa84df21b7116212b4ffded97ca293901
Nathalia Pereira <nathalia.pereira252000@gmail.com>
2021-05-26 15:11:31 -0300

    Implementa a tabela User com validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   has_one_attached :photo
 
-  #Confere se o usuário cadastrou o nome e email
+  #Confere se o usuário cadastrou nome e email único
+  #Usa a gem email-validator para conferir se o email possui o @ com caracters antes e depois dele
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, email: true
 
   #Confere se a senha do usuário tem pelo menos 8 caracteres
   validates :password, length: {minimum: 8}
-  
-  #Confere se o email do usuário já está cadastrado
-  validates :email, uniqueness: true
-  #Usa a gem email-validator para conferir se o email possui o @ com caracters antes e depois dele
-  validates :email, email: true
 
 end
