48dafdeba96dfb5829dd94f2d22940b3544d3b8c
Nathalia Pereira <nathalia.pereira252000@gmail.com>
2021-05-29 01:31:36 -0300

    feat: Implementa autenticação de usuário com JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  def not_found
+    render status: :not_found
+  end
+      
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      decoded = JsonWebToken.decode(header)
+      current_user = User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render status: :unauthorized
+    rescue JWT::DecodeError => e
+      render status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#find'
+  get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
   delete '/users/:id', to: 'users#destroy'
   put '/users/:id', to: 'users#update'
+
+  resources :users, param: :_name
+  post '/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
