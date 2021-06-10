e0d1985cd3df80456b0bf517e62ca2a68241be22
Nathalia Pereira <nathalia.pereira252000@gmail.com>
2021-06-10 13:51:26 -0300

    feat: Implementa tabelas de comentários e repostas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :replies
+  resources :comments
   resources :tags
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
@@ -11,6 +13,24 @@ Rails.application.routes.draw do
   post '/posts', to: 'posts#create'
   delete '/posts/:id', to: 'posts#destroy'
   put '/posts/:id', to: 'posts#update'
+
+  get '/tags', to: 'tags#index'
+  get '/tags/:id', to: 'tags#show'
+  post '/tags', to: 'tags#create'
+  delete '/tags/:id', to: 'tags#destroy'
+  put '/tags/:id', to: 'tags#update'
+
+  get '/comments', to: 'comments#index'
+  get '/comments/:id', to: 'comments#show'
+  post '/comments', to: 'comments#create'
+  delete '/comments/:id', to: 'comments#destroy'
+  put '/comments/:id', to: 'comments#update'
+
+  get '/replies', to: 'replies#index'
+  get '/replies/:id', to: 'replies#show'
+  post '/replies', to: 'replies#create'
+  delete '/replies/:id', to: 'replies#destroy'
+  put '/replies/:id', to: 'replies#update'
 
   resources :users, params: :name
   post '/login', to: 'authentication#login'
